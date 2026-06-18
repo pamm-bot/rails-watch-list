d8282369e2f80248714382a057bf97afb4c481cb
Pamela Morganti <pahelemoba@gmail.com>
2026-06-18 22:09:59 +0200

    Set lists#index as the root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root "lists#index"
+
   resources :lists, only: [ :index, :show, :create ]  do
     resources :bookmarks, only: [ :create ]
     resources :reviews, only: [ :create ]
